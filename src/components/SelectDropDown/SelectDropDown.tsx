7e7bb499a2adf3714d29a6e71a6f0aec838c2f70
niccololampa <niccololampa@gmail.com>
2022-09-07 20:04:14 +0800

    Update SelectDropDown component styling

--- src/components/SelectDropDown/SelectDropDown.tsx
+++ src/components/SelectDropDown/SelectDropDown.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import MenuItem from "@mui/material/MenuItem"
 import FormControl from "@mui/material/FormControl"
+import { styled } from "@mui/material/styles"
 import Select, { SelectChangeEvent } from "@mui/material/Select"
 
 const SelectDropDown = ({ category }: { category: string }) => {
@@ -10,20 +11,31 @@ const SelectDropDown = ({ category }: { category: string }) => {
     setItemSelected(event.target.value)
   }
 
+  // FIXME:  these can be set up in themes page color
+  const baseColor = "#768085"
+  const highlightColor = "#eeeea5"
+
+  const StyledFormControl = styled(FormControl)({
+    m: 1,
+    minWidth: 120,
+    "& .MuiInputBase-root": { color: baseColor },
+    "& .MuiSvgIcon-root": { color: baseColor },
+    "& .MuiOutlinedInput-notchedOutline": { borderColor: baseColor },
+    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+      borderColor: highlightColor,
+    },
+    "& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline": {
+      borderColor: highlightColor,
+    },
+  })
+
   return (
     <div>
-      <FormControl sx={{ m: 1, minWidth: 120 }}>
-        <Select
-          labelId="select-label"
-          id="select-helper"
-          value={itemSelected}
-          label={category}
-          onChange={handleChange}
-          displayEmpty
-        >
+      <StyledFormControl sx={{ m: 1, minWidth: 120 }}>
+        <Select id="select-helper" value={itemSelected} onChange={handleChange} displayEmpty>
           <MenuItem value="">None</MenuItem>
         </Select>
-      </FormControl>
+      </StyledFormControl>
     </div>
   )
 }
