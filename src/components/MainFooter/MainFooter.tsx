daf372cb54d6d3a0bb1139c8aff5537ed598f809
niccololampa <niccololampa@gmail.com>
2022-09-08 13:09:58 +0800

    Add initial MainFooter component

---
+++ src/components/MainFooter/MainFooter.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { HRBorder } from "../../components"
+
+const MainFooter = () => {
+  return (
+    <div>
+      <HRBorder />
+    </div>
+  )
+}
+
+export default MainFooter
