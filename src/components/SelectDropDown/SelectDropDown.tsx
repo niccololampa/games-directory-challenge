ef087983dc2fa2b8fae14d73635d948de518df1e
niccololampa <niccololampa@gmail.com>
2022-09-12 13:32:45 +0800

    Remove unused variable SelectDropDown

--- src/components/SelectDropDown/SelectDropDown.tsx
+++ src/components/SelectDropDown/SelectDropDown.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import { MenuItem, FormControl } from "@mui/material"
 import Select, { SelectChangeEvent } from "@mui/material/Select"
 import { styled } from "@mui/material/styles"
