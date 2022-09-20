9fdd1b240ad3050bed3d12dc770fc3fd8623be0d
niccololampa <niccololampa@gmail.com>
2022-09-20 19:47:52 +0800

    Add PageHeader2 styled file

--- src/components/PageHeader2/PageHeader2.tsx
+++ src/components/PageHeader2/PageHeader2.tsx
@@ -1,11 +1,5 @@
 import * as React from "react"
-import { styled } from "@mui/material/styles"
-import { Typography } from "@mui/material"
-
-const StyledTypography = styled(Typography)(() => ({
-  fontSize: "22px",
-  fontWeight: "bold",
-}))
+import { StyledTypography } from "./PageHeader2Styled"
 
 const PageHeader2 = ({ title }: { title: string }) => {
   return <StyledTypography variant="h2">{title}</StyledTypography>
---
+++ src/components/PageHeader2/PageHeader2Styled.tsx
@@ -0,0 +1,7 @@
+import { styled } from "@mui/material/styles"
+import { Typography } from "@mui/material"
+
+export const StyledTypography = styled(Typography)(() => ({
+  fontSize: "22px",
+  fontWeight: "bold",
+}))
