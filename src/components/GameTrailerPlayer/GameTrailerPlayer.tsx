21a2e2555c562bdf1d8fe5fb03c6d009a419ea79
niccololampa <niccololampa@gmail.com>
2022-09-20 19:02:46 +0800

    Update GameTrailerPlayer to have styled file

--- src/components/GameTrailerPlayer/GameTrailerPlayer.tsx
+++ src/components/GameTrailerPlayer/GameTrailerPlayer.tsx
@@ -1,15 +1,6 @@
 import React from "react"
 import ReactPlayer from "react-player"
-import { styled } from "@mui/material/styles"
-import { Box } from "@mui/material"
-
-const StyledTrailerBox = styled(Box)({
-  backgroundColor: "#3f3f3f",
-  marginTop: "20px",
-  borderRadius: "2%",
-  width: "100%",
-  height: "600px",
-})
+import { StyledTrailerBox } from "./GameTrailerPlayerStyled"
 
 const GameTrailerPlayer = ({ url }: { url: string }) => {
   return (
---
+++ src/components/GameTrailerPlayer/GameTrailerPlayerStyled.tsx
@@ -0,0 +1,10 @@
+import { styled } from "@mui/material/styles"
+import { Box } from "@mui/material"
+
+export const StyledTrailerBox = styled(Box)({
+  backgroundColor: "#3f3f3f",
+  marginTop: "20px",
+  borderRadius: "2%",
+  width: "100%",
+  height: "600px",
+})
