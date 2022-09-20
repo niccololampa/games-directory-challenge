a1cba9a5799fbec5a6747727fcce861e0a38a1c4
niccololampa <niccololampa@gmail.com>
2022-09-20 19:45:24 +0800

    Update PageHeader1 to have styled file

--- src/components/PageHeader1/PageHeader1.tsx
+++ src/components/PageHeader1/PageHeader1.tsx
@@ -1,11 +1,5 @@
 import * as React from "react"
-import { styled } from "@mui/material/styles"
-import { Typography } from "@mui/material"
-
-const StyledTypography = styled(Typography)(() => ({
-  fontSize: "35px",
-  fontWeight: "bold",
-}))
+import { StyledTypography } from "./PageHeader1Styled"
 
 const PageHeader1 = ({ title }: { title: string }) => {
   return <StyledTypography variant="h1">{title}</StyledTypography>
