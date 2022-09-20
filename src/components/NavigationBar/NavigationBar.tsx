0cdef363209cd4c31da33eea939f9b4cd077ee87
niccololampa <niccololampa@gmail.com>
2022-09-20 19:39:42 +0800

    Update Navigation Bar imports

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,16 +1,18 @@
 import React from "react"
 import { SocialMediaLinks, PageLinks, EarnAllianceLogo } from "../../components"
-import AppBar from "@mui/material/AppBar"
-import Box from "@mui/material/Box"
-import Toolbar from "@mui/material/Toolbar"
-import IconButton from "@mui/material/IconButton"
-import Typography from "@mui/material/Typography"
-import Menu from "@mui/material/Menu"
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  Container,
+  Avatar,
+  Tooltip,
+  MenuItem,
+} from "@mui/material"
 import MenuIcon from "@mui/icons-material/Menu"
-import Container from "@mui/material/Container"
-import Avatar from "@mui/material/Avatar"
-import Tooltip from "@mui/material/Tooltip"
-import MenuItem from "@mui/material/MenuItem"
 import HRBorder from "../HRBorder/HRBorder"
 import "./NavigationBar.css"
 import earnAllianceLogo from "../../earn-alliance-logo.png"
