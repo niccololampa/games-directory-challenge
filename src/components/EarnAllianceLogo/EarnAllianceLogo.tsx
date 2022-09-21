791a14e6d00b833e139a7a451855fd2812c81e23
niccololampa <niccololampa@gmail.com>
2022-09-21 16:19:47 +0800

    Add navigate to home upon click of EarnAlliance Logo

--- src/components/EarnAllianceLogo/EarnAllianceLogo.tsx
+++ src/components/EarnAllianceLogo/EarnAllianceLogo.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { useNavigate } from "react-router-dom"
 import { Box } from "@mui/material"
 import earnAllianceLogo from "../../earn-alliance-logo.png"
 
@@ -8,14 +9,24 @@ const EarnAllianceLogo = ({
 }: {
   width: string | number
   height: string | number
-}) => (
-  <Box
-    component="img"
-    alt="Earn Alliance Logo"
-    src={earnAllianceLogo}
-    width={width}
-    height={height}
-  />
-)
+}) => {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate("/")
+  }
+
+  return (
+    <Box
+      component="img"
+      alt="Earn Alliance Logo"
+      src={earnAllianceLogo}
+      width={width}
+      height={height}
+      onClick={handleClick}
+      sx={{ cursor: "pointer" }}
+    />
+  )
+}
 
 export default EarnAllianceLogo
